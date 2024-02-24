d7ed798faaff3d0020120d4853dae3c8a3806683
RicardoVerp <cmicveracruz17@hotmail.com>
2024-02-23 22:35:38 -0600

    primer Versión

--- src/components/EventForm/EventForm.jsx
+++ src/components/EventForm/EventForm.jsx
@@ -101,7 +101,26 @@ const handleSubmit = (e) => {
     
  setShowModal(false);
  setFormData({
-...formData
+...formData,
+name_complete:'',
+email:'',
+tel_oficina:'',
+num_cel:'',
+nom_emp:'',
+afil_cmic:'',
+del_cmic:'',
+int_afil:'',
+handicap:'',
+talla:'',
+carrito:'',
+factura:'',
+razon_social:'',
+rfc_fact:'',
+dom_fact:'',
+cod_Fiscal:'',
+cfdi_fact:'',
+regFiscal_fact:'',
+email_fact:''
 
  })
 
