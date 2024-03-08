8e2298aeb92edde4976ac19f291ae03c70135aaa
Ricardo <rnavarrete_cervantes@hotmail.com>
2024-03-07 18:16:47 -0600

    Update RegistroExitoso.jsx

--- src/components/EventDisplay/RegistroExitoso.jsx
+++ src/components/EventDisplay/RegistroExitoso.jsx
@@ -11,7 +11,7 @@ const RegistroExitoso = () => {
       <div className='descrip-Registro'>
         <h2>Gracias por tu Registro.</h2>
 
-<p>La Cámara Mexicana de la Industria de la Construcción Agradece tu Registro al 2do, Torneo de Golf.</p>
+<p>La Cámara Mexicana de la Industria de la Construcción Agradece tu Registro al 2do. Torneo de Golf.</p>
 
 <p>Vía correo electrónico le llegaran los datos bancarios y el proceso a seguir para el pago de su registro.</p>
       </div>
