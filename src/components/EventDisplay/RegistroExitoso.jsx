3fcdc0b15cf658926e557b6b57e0ac6ca72be9a9
RicardoVerp <cmicveracruz17@hotmail.com>
2024-03-18 17:39:36 -0600

    Se añade componentes login y dashboard

--- src/components/EventDisplay/RegistroExitoso.jsx
+++ src/components/EventDisplay/RegistroExitoso.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import {Banner}  from '../Banner/Banner.jsx';
-<<<<<<< HEAD
-
-=======
-import {Footer}  from '../Footer/Footer.jsx';
->>>>>>> 8e2298aeb92edde4976ac19f291ae03c70135aaa
 import './RegistroExitoso.css'
 
 
@@ -14,24 +9,11 @@ const RegistroExitoso = () => {
 <Banner />
       <div className='descrip-Registro'>
         <h2>Gracias por tu Registro.</h2>
-
-<<<<<<< HEAD
 <p>La Cámara Mexicana de la Industria de la Construcción Agradece tu Registro al 2do, Torneo de Golf.</p>
-=======
-<p>La Cámara Mexicana de la Industria de la Construcción Agradece tu Registro al 2do. Torneo de Golf.</p>
->>>>>>> 8e2298aeb92edde4976ac19f291ae03c70135aaa
-
 <p>Vía correo electrónico le llegaran los datos bancarios y el proceso a seguir para el pago de su registro.</p>
-      </div>
-        
-
-<<<<<<< HEAD
-=======
-      <Footer />
->>>>>>> 8e2298aeb92edde4976ac19f291ae03c70135aaa
-    </div>
-    
-  );
+</div>
+</div>
+);
 };
 
 export default RegistroExitoso;
